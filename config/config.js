5488b27df65b7a3f325ea10a64870f8267c58aa8
小橙子 <a2742217795@hotmail.com>
2023-05-24 00:02:38 +0800

    chore: 添加自动提交校验

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ const pkg_entry = require(pkg)["rx-entry"]
 const resolvePath = path.join(__dirname, '../') /*指定相对目录*/
 
 // 指定入口文件
-const rx_entry = path.join(resolvePath, 'src' ,'index.js')
+const rx_entry = path.join(resolvePath, pkg_entry ,'index.js')
 if(!rx_entry){
   utils.exit('使用模板的 index.js 文件不存');
 }
