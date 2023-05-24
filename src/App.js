5c8a2d87d21a7e172a9bdce25f26f7cd26f7d650
小橙子 <a2742217795@hotmail.com>
2023-05-24 21:47:02 +0800

    feat: add react_18

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React from 'react'
-import { Provider } from 'react-redux'
-import store from './store/index.js'
-import Home from './Application/Home/index'
-import { HashRouter } from 'react-router-dom'
-import { renderRoutes } from 'react-router-config'
-import routes from './router/index.js'
-const App = () => {
-  return (
-    <Provider store={store}>
-      <HashRouter>
-        <Home>{renderRoutes(routes)}</Home>
-      </HashRouter>
-    </Provider>
-  )
+
+function App() {
+  return <div className="App">App 组件</div>
 }
+
 export default App
--- src/store/reducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-// 接收容器组件的 reducer 然后到这里合并
-// import { reducer as MainPageReducers } from '../applications/MainPage/store/index';
-
-// 合并
-export default combineReducers({})
